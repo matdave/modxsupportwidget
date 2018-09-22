81d16b5985f86c5ebf7254bf7cb7d9819aec8844
Ivan Pokopenko <ask@colorwebdesigner.ru>
2018-09-23 02:11:34 +0300

    Add new lexicon translation for RU language

---
+++ core/components/modxsupportwidget/lexicon/ru/default.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created with Atom
+ * User: colorwebdesigner
+ * Project: modxsupportwidget
+ * Date: 23/09/2018
+ * https://github.com/colorwebdesigner
+ */
+
+$_lang['modxsupport.widget'] = 'Команда поддержки MODX';
+$_lang['modxsupport.widget.desc'] = 'Виджет позволяет отправить запрос в поддержку MODX из панели администратора';
+$_lang['modxsupport.widget.intro'] = '<p>Нужна помощь по сайту? Напишите в службу поддержки MODX. <br/><span class="x-combo-list-small">Мы оказываем квалифицированную техническую поддержку, поможем обновить систему или установить необходимые модули. <br/>За некоторые услуги может взиматься плата.</span></p>';
+$_lang['modxsupport.widget.fullname'] = 'Ваше имя';
+$_lang['modxsupport.widget.email'] = 'Ваш Email';
+$_lang['modxsupport.widget.message'] = 'Что нужно сделать';
+$_lang['modxsupport.widget.submit'] = 'Отправляем...';
+$_lang['modxsupport.widget.err_no_fullname'] = 'Вы не указали имя!';
+$_lang['modxsupport.widget.err_no_email'] = 'Вы не указали Email!';
+$_lang['modxsupport.widget.err_no_message'] = 'Вы не описали задачу!';
+$_lang['modxsupport.widget.success'] = 'Отправлено!';
